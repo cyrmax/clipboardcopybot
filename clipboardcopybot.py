2a43f7dd37ac8c4952a77ad255aec44be212c8f8
cyrmax <belousov.k.m@ya.ru>
2021-11-20 00:02:31 +0300

    Modifications in config

--- clipboardcopybot.py
+++ clipboardcopybot.py
@@ -1,21 +1,25 @@
-from platform import win32_is_iot
 import telebot
 from telebot import types
 from time import time
 import clipboard
 import winsound
 import configparser
+import json
 
 startTime = time()
 config = configparser.ConfigParser()
 config.read("config.ini")
+
+usernames = []
+usernames.append(config.get("bot", "username"))
+usernames += json.loads(config.get("bot", "additionalUsers"))
 
 bot = telebot.TeleBot(config.get("bot", "token"))
 
 @bot.message_handler(content_types=["text"])
 def getTextMessage(message):
   if message.date < startTime: return
-  if message.from_user.username in ["cyrmax", "daniil_savenya", "gumerov_amir"]:
+  if message.from_user.username in usernames:
     clipboard.copy(message.text)
     winsound.PlaySound("sounds/copied.wav", winsound.SND_FILENAME)
     bot.send_message(message.from_user.id, "Copied!")
