e7cb70792697f196ae75395e1194fdc3e01fb5c9
Gumerov Amir Eduardovich <=>
2021-11-20 15:48:47 +0300

    not defined config file detecting, specifying handling offline messages

--- clipboardcopybot.py
+++ clipboardcopybot.py
@@ -1,14 +1,19 @@
-import telebot
-from telebot import types
-from time import time
-import clipboard
-import winsound
 import configparser
 import json
+import sys
+from time import time
+import winsound
 
-startTime = time()
+import clipboard
+import telebot
+from telebot import types
+
 config = configparser.ConfigParser()
 config.read("config.ini")
+if not config.has_section("bot"):
+    sys.exit("please, create config file \"config.ini\" before running the program")
+
+startTime = time() if not config.getboolean("bot", "handle_offline_messages") else 0
 
 usernames = []
 usernames.append(config.get("bot", "username"))
